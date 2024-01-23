9bdeb6caa2e1ba686c68beca6da7c6666e492089
Qurin3 <124502189+Qurin3@users.noreply.github.com>
2024-01-23 18:45:18 +0100

    zmiana na czerwony

--- car-rental/components/Navbar.tsx
+++ car-rental/components/Navbar.tsx
@@ -8,9 +8,12 @@ const Navbar = () => {
     <header className='w-full absolute z-10'>
       <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'>
         <Link href="/" className='flex justify-center items-center'>
-          <Image src="/logo.svg" alt='Car Hub Logo' width={118} height={18} className='object-contain'/>
+          <Image src="/altLogo.png" alt='Car Hub Logo' width={118} height={18} className='object-contain'/>
+          <span
+          className='text-xl px-5'
+          >Wynajem samochodów — Albert Pieniądz
+          </span>
         </Link>
-        <CustomButton title="Sign in" btnType="button" containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' />
       </nav>
     </header>
   )
